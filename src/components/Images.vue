d9fb4374679468e5bcabff5e214e2578113128a0
yaojie <limyaojie93@gmail.com>
2017-12-16 08:20:52 +0800

    + images

---
+++ src/components/Images.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class='images'>
+    <img src='../assets/1.jpg'>
+    <img src='../assets/2.jpg'>
+    <img src='../assets/3.jpg'>
+    <img src='../assets/4.jpg'>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Images',
+  data () {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
